81a86581300fb66f98fc361d2a0c995d575d4515
ilyass-bel <ilyassbelbaze@gmail.com>
2021-12-07 15:18:48 +0100

    Create a date attribute and crete a filter on that attribute

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -7,9 +7,12 @@ use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use DateTime;
 
 
 #[ApiFilter(SearchFilter::class, properties: ['nbPages' => 'exact', 'Title' => 'partial'] )]
+#[ApiFilter(DateFilter::class, properties: ['createdAt'])]
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource(
@@ -23,16 +26,20 @@ class Book
     private $id;
 
     #[ORM\Column(type: 'string', length: 100)]
-    private $Title;
+    private $title;
 
     #[ORM\Column(type: 'string', length: 50)]
-    private $Author;
+    private $author;
 
     #[ORM\Column(type: 'integer')]
     private $nbPages;
 
     #[ORM\Column(type: 'float')]
-    private $Price;
+    private $price;
+
+    #[ORM\Column(type: 'datetime')]
+    private $createdAt;
+
 
     public function getId(): ?int
     {
@@ -41,24 +48,24 @@ class Book
 
     public function getTitle(): ?string
     {
-        return $this->Title;
+        return $this->title;
     }
 
-    public function setTitle(string $Title): self
+    public function setTitle(string $title): self
     {
-        $this->Title = $Title;
+        $this->title = $title;
 
         return $this;
     }
 
     public function getAuthor(): ?string
     {
-        return $this->Author;
+        return $this->author;
     }
 
-    public function setAuthor(string $Author): self
+    public function setAuthor(string $author): self
     {
-        $this->Author = $Author;
+        $this->author = $author;
 
         return $this;
     }
@@ -77,12 +84,24 @@ class Book
 
     public function getPrice(): ?float
     {
-        return $this->Price;
+        return $this->price;
     }
 
-    public function setPrice(float $Price): self
+    public function setPrice(float $price): self
     {
-        $this->Price = $Price;
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTime
+   {
+        return $this->createdAt;
+   } 
+
+   public function setCreatedAt(DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
